09c6bbd4d5df4287a40d7ceca77319a01b4870a1
Luca-Lisai <luca.lisai@edu.esiee.fr>
2025-09-22 12:18:45 +0000

    exercice terminé

--- main.py
+++ main.py
@@ -1,21 +1,29 @@
+"""Exercice 6.1 - Nombres premiers"""
 from math import sqrt
 
 #### Fonction secondaire
+def isprime(valeur):
+    """Teste si une valeur est un nombre premier."""
+    premier = True
+    if valeur<2 :
+        premier = False
+    else:
+        for i in range (2, int(sqrt(valeur)+1)) :
+            if valeur % i == 0 :
+                premier = False
+                break
+    return premier
 
-
-def isprime(p):
-
-    # votre code ici
-
-    pass
 
 #### Fonction principale
-
-
 def main():
-
+    """Appelle la fonction secondaire.""" 
     # vos appels à la fonction secondaire ici
-
+    val = int(input("Choose a number : "))
+    if isprime(val) :
+        print(val, " est un nombre premier ")
+    else :
+        print(val, " n'est pas un nombre premier ")
     for n in range(100):
         if isprime(n):
             print(n, end=", ")
